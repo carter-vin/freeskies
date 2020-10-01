cf638751da36bf8939226e76af532465b5f6ebcf
EliteMarko <fast.dev.ninja@gmail.com>
2020-10-01 22:33:49 +0200

    - Configured next.js project with typescript, tailwindcss applied. - Began signup page.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# freeskies
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
---
+++ pages/signup.js
@@ -0,0 +1,9 @@
+export default function SignUp() {
+  return (
+    <div className="flex content-center w-full min-h-screen bg-gray-400">
+      <div className="m-auto" style={{ width: 410 }}>
+        <img src="/logo.png" style={{ height: 55 }} />
+      </div>
+    </div>
+  );
+}
