79e523f1807b53288ec26066546a9a383a91171d
EliteMarko <fast.dev.ninja@gmail.com>
2020-10-01 22:48:00 +0200

    Sign up page 10% done.

--- pages/signup.js
+++ pages/signup.js
@@ -1,8 +1,10 @@
 export default function SignUp() {
   return (
-    <div className="flex content-center w-full min-h-screen bg-gray-400">
-      <div className="m-auto" style={{ width: 410 }}>
-        <img src="/logo.png" style={{ height: 55 }} />
+    <div className="flex content-center w-full min-h-screen bg-blue-900">
+      <div className="m-auto flex flex-col items-center" style={{ width: 410 }}>
+        <img src="/logo.png" className="mb-32" style={{ height: 55 }} />
+        <input className="w-full text-lg" placeholder="First Name" />
+        <input className="w-full text-lg mt-4" placeholder="Last Name" />
       </div>
     </div>
   );
