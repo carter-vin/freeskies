3a8742fbe0e25437d1964321c2b0a793b3e6bf27
EliteMarko <fast.dev.ninja@gmail.com>
2020-10-02 19:41:52 +0200

    Gender selection part done for signup.

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import styles from "./signup.module.css";
+
 export default function SignUp() {
   const [step, setStep] = useState(0);
 
@@ -23,6 +25,42 @@ export default function SignUp() {
         />
       </div>
     );
+  } else if (step === 1) {
+    content = (
+      <div className="w-full">
+        <p className="text-lg text-gray-500 mb-1">Gender</p>
+        <div className="flex justify-between mb-4">
+          <div className="border border-gray-500 rounded-md flex-grow mr-2 p-2 px-3 flex items-center">
+            <label
+              for="male"
+              className="text-gray-500 flex-grow"
+              className={styles.container}
+            >
+              Male
+              <input
+                type="radio"
+                id="male"
+                name="gender"
+                value="male"
+                checked="checked"
+              />
+              <span className={styles.checkmark}></span>
+            </label>
+          </div>
+          <div className="border border-gray-500 rounded-md flex-grow ml-2 p-2 px-3 flex items-center">
+            <label
+              for="female"
+              className="text-gray-500 flex-grow"
+              className={styles.container}
+            >
+              Female
+              <input type="radio" id="female" name="gender" value="female" />
+              <span className={styles.checkmark}></span>
+            </label>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
@@ -31,7 +69,7 @@ export default function SignUp() {
         <img src="/logo.png" className="mb-32" style={{ height: 55 }} />
         {content}
         <button
-          className="w-full rounded-md bg-blue-700 h-12 mt-8 text-white text-lg"
+          className="w-full rounded-md bg-blue-700 h-12 mt-8 text-white text-lg font-medium"
           style={{ outline: "none" }}
           onClick={onHandleNext}
         >
