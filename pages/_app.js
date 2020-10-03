e8e31950873f1842f549e96db247a986c2f9d472
Gurami Niazashvili <gurami.niazshvili@gmail.com>
2020-10-03 14:29:44 +0400

    add styles file on page

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import "../css/tailwind.css";
+import '../css/tailwind.css';
+import 'antd/dist/antd.css';
+import '../css/global.scss';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
