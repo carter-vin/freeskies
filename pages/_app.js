4381335eb4babecaf8eccbd47d7b12ae076ddb9f
Gurami Niazashvili <gurami.niazshvili@gmail.com>
2020-10-03 18:44:46 +0400

    add title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import Head from 'next/head';
 import 'css/tailwind.css';
 import 'antd/dist/antd.css';
 import 'css/global.scss';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Freeskies</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
