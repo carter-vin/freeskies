39fdf73c0c1744a040b10b9bf84764a35d81c07f
Gurami Niazashvili <gurami.niazshvili@gmail.com>
2020-10-03 15:14:36 +0400

    update css path

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import '../css/tailwind.css';
+import 'css/tailwind.css';
 import 'antd/dist/antd.css';
-import '../css/global.scss';
+import 'css/global.scss';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
