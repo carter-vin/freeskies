0af4e5ba32bba5a1159e851222cfbeef72cfe3ae
Gurami Niazashvili <gurami.niazshvili@gmail.com>
2020-10-07 22:13:46 +0400

    create general wrapper for modal

---
+++ components/common/ModalWrapper.js
@@ -0,0 +1,37 @@
+import ReactModal from 'react-modal';
+import { CloseOutlined, CloseCircleFilled } from '@ant-design/icons';
+import styles from './styles/modal.module.scss';
+
+const customStyles = {
+  content: {
+    // width: '50%',
+    // left: '0',
+    // right: '0',
+    // margin: 'auto',
+  },
+  overlay: {
+    backgroundColor: 'rgba(50, 50, 50, 0.5)',
+  },
+};
+
+export default function ModalWrapper({ children, showModal, title, ...rest }) {
+  return (
+    <ReactModal
+      isOpen={showModal}
+      style={customStyles}
+      className={styles.modal_content}
+      shouldCloseOnOverlayClick
+      onRequestClose={rest.onClose}
+    >
+      <div className={styles.header}>
+        <div className={styles.title}>
+          <p>{title}</p>
+        </div>
+        <button className={styles.close} type="button" onClick={rest.onClose}>
+          <CloseCircleFilled />
+        </button>
+      </div>
+      <div className={styles.content}>{children}</div>
+    </ReactModal>
+  );
+}
