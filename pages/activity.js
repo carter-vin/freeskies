5adfbee3c5035f1f3c381e2fb676185ff8b22da3
Gurami Niazashvili <gurami.niazshvili@gmail.com>
2020-10-08 17:22:36 +0400

    create activity page

---
+++ pages/activity.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from 'components/header/Header';
+
+export default function activity() {
+  return (
+    <div>
+      <Header />
+      activity
+    </div>
+  );
+}
