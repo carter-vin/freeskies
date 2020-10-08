31c09afcd0ccb7736859608161c0fc4841d28087
Gurami Niazashvili <gurami.niazshvili@gmail.com>
2020-10-08 18:23:48 +0400

    add feed on posting post component

--- containers/Feed/Feed.js
+++ containers/Feed/Feed.js
@@ -1,5 +1,6 @@
 import Header from 'components/header/Header';
 import FeedPosts from 'components/profile/FeedPosts';
+import PostingPost from 'components/profile/PostingPost';
 import styles from './styles/feed.module.scss';
 
 export default function Feed() {
@@ -8,6 +9,7 @@ export default function Feed() {
       <Header />
       <div className={styles.feed}>
         <div className={styles.feed_container}>
+          <PostingPost />
           <FeedPosts />
         </div>
       </div>
