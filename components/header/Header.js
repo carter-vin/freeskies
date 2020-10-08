7a09fcceac3729fdd42b8ef754d2e4e516cf3045
Gurami Niazashvili <gurami.niazshvili@gmail.com>
2020-10-08 21:18:53 +0400

    header responsive

--- components/header/Header.js
+++ components/header/Header.js
@@ -2,8 +2,6 @@ import styles from './styles/header.module.scss';
 import { useRouter } from 'next/router';
 
 export default function Header({ withoutSearch }) {
-  const router = useRouter();
-
   return (
     <div className={styles.header}>
       <div className={styles.container}>
@@ -11,23 +9,36 @@ export default function Header({ withoutSearch }) {
           <img src="/logo.png" alt="logo" />
         </div>
         <div className={styles.search}>
-          {!withoutSearch && <input name="search" placeholder="Search ..." />}
+          {!withoutSearch && (
+            <>
+              <input name="search" placeholder="Search ..." />
+
+              <UserProfile />
+            </>
+          )}
         </div>
-        <div
-          className={styles.profile}
-          onClick={() => {
-            router.push('/profile');
-          }}
-        >
-          <div className={styles.avatar}>
-            <img
-              src="https://api.adorable.io/avatars/128/adorable.png"
-              alt="avatar"
-            />
-          </div>
-          <span className={styles.name}>John</span>
-        </div>
+        <UserProfile />
       </div>
+    </div>
+  );
+}
+
+function UserProfile() {
+  const router = useRouter();
+  return (
+    <div
+      className={styles.profile}
+      onClick={() => {
+        router.push('/profile');
+      }}
+    >
+      <div className={styles.avatar}>
+        <img
+          src="https://api.adorable.io/avatars/128/adorable.png"
+          alt="avatar"
+        />
+      </div>
+      <span className={styles.name}>John</span>
     </div>
   );
 }
