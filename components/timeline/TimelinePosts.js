269c1ecfae95fb912ff2af1be8064a975f5bd9f3
Gurami Niazashvili <gurami.niazshvili@gmail.com>
2020-10-13 00:02:56 +0400

    grid system for images in a post

--- components/timeline/TimelinePosts.js
+++ components/timeline/TimelinePosts.js
@@ -1,11 +1,17 @@
 import { LikeFilled, MessageFilled } from '@ant-design/icons';
 import styles from './styles/timeline-post.module.scss';
+import classnames from 'classnames';
+
+// temporary data
+const image1 = [0];
+const image2 = [0, 0];
+const image3 = [0, 0, 0];
 
 export default function TimelinePosts() {
   return (
     <div className={styles.activity_content}>
       {[0, 0, 0, 0].map((item, index) => (
-        <div className={styles.post}>
+        <div className={styles.post} key={index}>
           <div className={styles.post_header}>
             <div className={styles.avatar}>
               <img
@@ -26,11 +32,62 @@ export default function TimelinePosts() {
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s
             </p>
-            <div className={styles.image}>
-              <img
-                src="https://images.unsplash.com/photo-1519834785169-98be25ec3f84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-                alt=""
-              />
+            <div
+              className={classnames(styles.image, {
+                [styles.grid_2]: index === 1,
+                [styles.grid_3]: index === 2,
+              })}
+            >
+              {(index === 0 || index > 2) && (
+                <img
+                  src="https://images.unsplash.com/photo-1519834785169-98be25ec3f84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                  alt=""
+                />
+              )}
+
+              {index === 1 && (
+                <>
+                  <div
+                    className={classnames(styles.image_item, styles.main_photo)}
+                  >
+                    <img
+                      src="https://images.unsplash.com/photo-1519834785169-98be25ec3f84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                      alt=""
+                    />
+                  </div>
+                  <div className={styles.image_item}>
+                    <img
+                      src="https://images.unsplash.com/photo-1519834785169-98be25ec3f84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                      alt=""
+                    />
+                  </div>
+                </>
+              )}
+
+              {index === 2 && (
+                <>
+                  <div
+                    className={classnames(styles.image_item, styles.main_photo)}
+                  >
+                    <img
+                      src="https://images.unsplash.com/photo-1519834785169-98be25ec3f84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                      alt=""
+                    />
+                  </div>
+                  <div className={styles.image_item}>
+                    <img
+                      src="https://images.unsplash.com/photo-1519834785169-98be25ec3f84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                      alt=""
+                    />
+                  </div>
+                  <div className={styles.image_item}>
+                    <img
+                      src="https://images.unsplash.com/photo-1519834785169-98be25ec3f84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                      alt=""
+                    />
+                  </div>
+                </>
+              )}
             </div>
           </div>
           <div className={styles.post_actions}>
