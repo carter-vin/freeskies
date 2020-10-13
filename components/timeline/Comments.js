2f346de5b12908dd3c2020dc1c787b941d0cb830
Gurami Niazashvili <gurami.niazshvili@gmail.com>
2020-10-13 19:16:32 +0400

    update comment UI

--- components/timeline/Comments.js
+++ components/timeline/Comments.js
@@ -12,7 +12,16 @@ export default function Comments({ show }) {
             alt="avatar"
           />
         </div>
-        <div className={styles.message}>message</div>
+        <div className={styles.message_container}>
+          <div className={styles.message}>
+            <span className={styles.author}>John Doe</span>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s
+            </p>
+          </div>
+        </div>
       </div>
 
       <div className={classnames(styles.comment_item, styles.not_mine)}>
@@ -22,7 +31,10 @@ export default function Comments({ show }) {
             alt="avatar"
           />
         </div>
-        <div className={styles.message}>message</div>
+        <div className={styles.message}>
+          <span className={styles.author}>John Doe</span>
+          <p>message</p>
+        </div>
       </div>
     </div>
   );
