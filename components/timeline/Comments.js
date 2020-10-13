340bd1f591ea0667d769b4e8b2c28cef3bf492a9
Gurami Niazashvili <gurami.niazshvili@gmail.com>
2020-10-13 19:04:59 +0400

    update comment style for my comment

--- components/timeline/Comments.js
+++ components/timeline/Comments.js
@@ -1,12 +1,24 @@
 import styles from './styles/comment.module.scss';
+import classnames from 'classnames';
 
-export default function Comments() {
+export default function Comments({ show }) {
+  if (!show) return null;
   return (
     <div className={styles.comments}>
       <div className={styles.comment_item}>
         <div className={styles.avatar}>
           <img
             src={`https://api.adorable.io/avatars/50/adorable.png`}
+            alt="avatar"
+          />
+        </div>
+        <div className={styles.message}>message</div>
+      </div>
+
+      <div className={classnames(styles.comment_item, styles.not_mine)}>
+        <div className={styles.avatar}>
+          <img
+            src={`https://api.adorable.io/avatars/50/adorable2.png`}
             alt="avatar"
           />
         </div>
