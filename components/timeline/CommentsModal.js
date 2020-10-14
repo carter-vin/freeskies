cc94ae5d14771f8120a30bdef325ccf41e2c3c43
Gurami Niazashvili <gurami.niazshvili@gmail.com>
2020-10-15 01:07:42 +0400

    clear unused variable

--- components/timeline/CommentsModal.js
+++ components/timeline/CommentsModal.js
@@ -1,13 +1,4 @@
-import AliceCarousel from 'react-alice-carousel';
 import ModalWrapper from 'components/common/ModalWrapper';
-import { useState } from 'react';
-import {
-  LeftCircleFilled,
-  RightCircleFilled,
-  LeftOutlined,
-  RightOutlined,
-} from '@ant-design/icons';
-import { RatingSlide } from 'components/forms';
 import Comments from './Comments';
 
 export default function CommentsModal({ ...rest }) {
