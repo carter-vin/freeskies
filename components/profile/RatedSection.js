6b6abbaadafd634e921aace739282a492cef7cc1
Gurami Niazashvili <gurami.niazshvili@gmail.com>
2020-10-16 22:40:12 +0400

    add action on image

--- components/profile/RatedSection.js
+++ components/profile/RatedSection.js
@@ -2,8 +2,14 @@ import React from 'react';
 import classnames from 'classnames';
 import { Button, Rate } from 'antd';
 import styles from './styles/rated-section.module.scss';
+import PhotosModal from './PhotosModal';
+import { useModal } from 'react-modal-hook';
 
 export default function RatedSection() {
+  const [showModal, hideModal] = useModal(({ in: open }) => (
+    <PhotosModal title="Photo detail" showModal={open} onClose={hideModal} />
+  ));
+
   return (
     <div className={classnames(styles.container_bg, styles.section)}>
       <div className={styles.section_title}>
@@ -14,7 +20,7 @@ export default function RatedSection() {
         {[0, 0, 0, 0, 0].map((item, index) => {
           return (
             <div className={styles.log_item} key={index}>
-              <div className={styles.image}>
+              <div className={styles.image} onClick={showModal}>
                 <img
                   src={`https://images.unsplash.com/photo-1519834785169-98be25ec3f84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80`}
                   alt=""
