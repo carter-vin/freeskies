ea9b5a5ceadc9c1bd236f63590d289ba925afc95
Gurami Niazashvili <gurami.niazshvili@gmail.com>
2020-10-19 22:33:31 +0400

    update loading component

--- components/common/LoadingWrapper.js
+++ components/common/LoadingWrapper.js
@@ -1,6 +1,12 @@
+import { LoadingOutlined } from '@ant-design/icons';
+
 export default function LoadingWrapper({ loading, children }) {
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div style={{ margin: '2em', textAlign: 'center' }}>
+        <LoadingOutlined style={{ fontSize: '2em', color: '#407cff' }} />
+      </div>
+    );
   }
   return children;
 }
