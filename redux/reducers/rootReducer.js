8790f2096d8cc255f21a00cfe19a266923a8a981
Gurami Niazashvili <gurami.niazshvili@gmail.com>
2020-10-21 20:34:11 +0400

    update for modal

--- redux/reducers/rootReducer.js
+++ redux/reducers/rootReducer.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 import appReducer from './appReducer';
+import { reducer as modal } from 'redux-modal';
+
 import nextReducer from './nextReducer';
 
 const rootReducer = combineReducers({
   // next: nextReducer,
   app: appReducer,
+  modal,
 });
 
 export default rootReducer;
