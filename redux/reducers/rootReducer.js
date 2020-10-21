c33f0437669d23add973ab643701186c5c9cc8c1
Gurami Niazashvili <gurami.niazshvili@gmail.com>
2020-10-21 23:12:36 +0400

    add auth controller reducer

--- redux/reducers/rootReducer.js
+++ redux/reducers/rootReducer.js
@@ -3,9 +3,11 @@ import appReducer from './appReducer';
 import { reducer as modal } from 'redux-modal';
 
 import nextReducer from './nextReducer';
+import authReducer from './authReducer';
 
 const rootReducer = combineReducers({
   // next: nextReducer,
+  auth: authReducer,
   app: appReducer,
   modal,
 });
